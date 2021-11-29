72f197a323f61c0568ed9e0aa77fc0ac48149be8
Tamaki <wenxuan10000@gmail.com>
2021-11-29 17:45:17 +0800

    Lmao

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def change_direction(new_direction):    ### DARRYL
      pass
      
 def check_collision(snake):   ### DARRYL
-     print('poggers')
+     pass
 
 def game_over(): ### JADEN
      print('gg')
