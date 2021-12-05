100ab49970a30fea68a3a73ce8d2c9cd5a9817f3
Raynard <raynardchai@outlook.com>
2021-12-05 14:52:04 +0800

    updated with new function, updated readme

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import random
 # Global Constants
 # -------------------------------------------------
 
-GAME_WIDTH = 1500
-GAME_HEIGHT = 1500
-SPACE_SIZE = 150
+GAME_WIDTH = 1000
+GAME_HEIGHT = 1000
+SPACE_SIZE = 100
 
 ROWS = int(GAME_HEIGHT/SPACE_SIZE)
 COLS = int(GAME_WIDTH/SPACE_SIZE)
@@ -22,6 +22,7 @@ YELLOW_COLOR = "#FBF46D"
 ORANGE_COLOR = '#FF4848'
 BG_COLOR = '#F9F3DF'
 TEXT_COLOR = "#7BC043"
+BLACK_COLOR = "#000000"
 
 class MakanTime:
      # -------------------------------------------------
@@ -32,7 +33,7 @@ class MakanTime:
      def __init__(self):
           ## create window settings
           self.window = Tk()
-          self.window.title("Makan Time")
+          self.window.title("Makan-Time")
           self.window.resizable(False, False)
 
           ## create canvas
@@ -52,16 +53,19 @@ class MakanTime:
           ## obj list used to generate display
           self.poison_obj = []
           self.food_obj = []
+          self.wall_obj = []
 
           ## cell list used to identify coordinate of obj
           self.old_poison_cell = []
           self.old_food_cell = []
+          self.previous_wall_cells = []
 
-          ## draw lines to create grid
+          ## create board
           for i in range(ROWS):
                for j in range(COLS):
                     self.board.append((i, j))
 
+          ## draw lines to create grid
           for i in range(ROWS):
                self.canvas.create_line(
                     i * GAME_WIDTH / ROWS, 0, i * GAME_WIDTH / ROWS, GAME_WIDTH,
@@ -82,10 +86,10 @@ class MakanTime:
           ## create dict of forbidden actions
           ## prevent snake from eating itself
           self.forbidden_actions = {}
-          self.forbidden_actions["Right"] = "Left"
-          self.forbidden_actions["Left"] = "Right"
           self.forbidden_actions["Up"] = "Down"
           self.forbidden_actions["Down"] = "Up"
+          self.forbidden_actions["Right"] = "Left"
+          self.forbidden_actions["Left"] = "Right"
 
           self.snake_objects = []
           for i in range(BODY_PARTS):
@@ -147,7 +151,7 @@ class MakanTime:
 
      # MALCOM
      def place_food(self):
-          unoccupied_cells = set(self.board) - set(self.snake) - set(self.old_poison_cell)
+          unoccupied_cells = set(self.board) - set(self.snake) - set(self.old_poison_cell) - set(self.previous_wall_cells)
           self.food_cell = random.choice(list(unoccupied_cells))
           x = self.food_cell[0] * SPACE_SIZE
           y = self.food_cell[1] * SPACE_SIZE
@@ -157,7 +161,7 @@ class MakanTime:
 
      # MALCOM
      def place_poison(self):
-          unoccupied_cells = set(self.board) - set(self.snake) - set(self.old_food_cell)
+          unoccupied_cells = set(self.board) - set(self.snake) - set(self.old_food_cell) - set(self.previous_wall_cells)
           self.poison_cell = random.choice(list(unoccupied_cells))
           x = self.poison_cell[0] * SPACE_SIZE
           y = self.poison_cell[1] * SPACE_SIZE
@@ -165,6 +169,17 @@ class MakanTime:
                x, y, x+SPACE_SIZE, y+SPACE_SIZE, fill=YELLOW_COLOR, outline=ORANGE_COLOR, width=5, tag="poison"
           )
 
+     ## JADEN
+     def place_wall(self):
+          unoccupied_cells = set(self.board) - set(self.snake) - set(self.old_food_cell) - set(self.previous_wall_cells)
+          self.wall_cell = random.choice(list(unoccupied_cells))
+          x = self.wall_cell[0] * SPACE_SIZE
+          y = self.wall_cell[1] * SPACE_SIZE
+          self.wall_obj = self.canvas.create_rectangle(
+               x, y, x+SPACE_SIZE, y+SPACE_SIZE, fill=BLACK_COLOR, outline=BLACK_COLOR, width=5, tag="wall"
+          )
+          self.previous_wall_cells.append(self.wall_cell)
+     
      # RAYNARD
      def display_snake(self, mode=""):
           ## prevent snake from growing without eating, pop every step
@@ -231,14 +246,15 @@ class MakanTime:
                else: self.snake.pop(0)
           
           ## append new head depending on direction of movement
-          if key == "Left":
-               self.snake.append((head[0] - 1, head[1]))
-          elif key == "Right":
-               self.snake.append((head[0] + 1, head[1]))
-          elif key == "Up":
+          if key == "Up":
                self.snake.append((head[0], head[1] - 1))
           elif key == "Down":
                self.snake.append((head[0], head[1] + 1))
+          elif key == "Left":
+               self.snake.append((head[0] - 1, head[1]))
+          elif key == "Right":
+               self.snake.append((head[0] + 1, head[1]))
+          
 
           head = self.snake[-1]
           if (
@@ -250,6 +266,8 @@ class MakanTime:
                or head[1] > ROWS - 1
                ## exceed bottom side of border
                or head[1] < 0
+               ## head hits walls
+               or head in set(self.previous_wall_cells)
                ## if len not equal means snake body part overlaps
                or len(set(self.snake)) != len(self.snake)
                ## 0 body parts left
@@ -258,14 +276,15 @@ class MakanTime:
                # hit border/ own body/ or no more body
                self.crashed = True
 
-          elif self.food_cell == head:
+          elif head == self.food_cell:
                # eat food
                self.old_food_cell = self.food_cell
                self.canvas.delete(self.food_obj)
                self.place_food()
+               self.place_wall()
                self.display_snake()
 
-          elif self.poison_cell == head:
+          elif head == self.poison_cell:
                # eat poison
                self.old_poison_cell = self.poison_cell
                self.canvas.delete(self.poison_obj)
@@ -304,5 +323,5 @@ class MakanTime:
                     self.last_key = key_pressed
 
 
-main_instance = MakanTime()
-main_instance.mainloop()
+game = MakanTime()
+game.mainloop()
