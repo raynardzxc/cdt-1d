ee3c00432ccf11161281e660bf001a7ab649edce
Raynard <raynardchai@outlook.com>
2021-12-05 13:46:23 +0800

    fixed bugs and included work allocation

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import random
 # Global Constants
 # -------------------------------------------------
 
-GAME_WIDTH = 700
-GAME_HEIGHT = 700
-SPACE_SIZE = 50
+GAME_WIDTH = 1500
+GAME_HEIGHT = 1500
+SPACE_SIZE = 150
 
 ROWS = int(GAME_HEIGHT/SPACE_SIZE)
 COLS = int(GAME_WIDTH/SPACE_SIZE)
@@ -27,45 +27,37 @@ class MakanTime:
      # -------------------------------------------------
      # Init Functions
      # -------------------------------------------------
+
+     # DARRYL
      def __init__(self):
+          ## create window settings
           self.window = Tk()
           self.window.title("Makan Time")
-
-          # ## get game dimensions
-          # window_width = self.window.winfo_width()
-          # window_height = self.window.winfo_height()
-
-          # ## get screen dimension
-          # screen_width = self.window.winfo_screenwidth()
-          # screen_height = self.window.winfo_screenheight()
-
-          # ## get coordinate of where game screen should be
-          # x = int((screen_width/2) - (window_width/2))
-          # y = int((screen_height/2) - (window_height/2))
-
-          # ## set game window
-          # self.window.geometry(f"{window_width}x{window_height}+{x}+{y}")
-
           self.window.resizable(False, False)
 
+          ## create canvas
           self.canvas = Canvas(self.window, bg=BG_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
           self.canvas.pack()
           
-          
-          
-          ## Binding User Input
+          ## binding sser input
           self.window.bind("<Key>", self.key_input)
           self.window.bind("<Button-1>", self.mouse_input)
           self.play_again()
           self.begin = False
      
+     # DARRYL
      def initialize_board(self):
           self.board = []
+
+          ## obj list used to generate display
           self.poison_obj = []
           self.food_obj = []
+
+          ## cell list used to identify coordinate of obj
           self.old_poison_cell = []
           self.old_food_cell = []
 
+          ## draw lines to create grid
           for i in range(ROWS):
                for j in range(COLS):
                     self.board.append((i, j))
@@ -80,28 +72,35 @@ class MakanTime:
                     0, i * GAME_HEIGHT / COLS, GAME_HEIGHT, i * GAME_HEIGHT / COLS,
                     )
      
+     # DARRYL
      def initialise_snake(self):
           self.snake = []
           self.crashed = False
-          self.heading = "Right"
+          self.heading = "Down"
           self.last_key = self.heading
+
+          ## create dict of forbidden actions
+          ## prevent snake from eating itself
           self.forbidden_actions = {}
           self.forbidden_actions["Right"] = "Left"
           self.forbidden_actions["Left"] = "Right"
           self.forbidden_actions["Up"] = "Down"
           self.forbidden_actions["Down"] = "Up"
+
           self.snake_objects = []
           for i in range(BODY_PARTS):
-               self.snake.append((i, 0))
+               self.snake.append((0, i))
 
+     # HARITHA
      def play_again(self):
           self.canvas.delete("all")
           self.initialize_board()
           self.initialise_snake()
           self.place_food()
           self.place_poison()
-          self.display_snake(mode="full")
+          self.display_snake(mode="init")
 
+     # HARITHA
      def mainloop(self):
           while True:
                self.window.update()
@@ -116,6 +115,7 @@ class MakanTime:
      # Display Functions
      # -------------------------------------------------
      
+     # HARITHA
      def display_gameover(self):
           score = len(self.snake)
           self.canvas.delete("all")
@@ -145,6 +145,7 @@ class MakanTime:
                text = play_text,
           )
 
+     # MALCOM
      def place_food(self):
           unoccupied_cells = set(self.board) - set(self.snake) - set(self.old_poison_cell)
           self.food_cell = random.choice(list(unoccupied_cells))
@@ -154,6 +155,7 @@ class MakanTime:
                x, y, x+SPACE_SIZE, y+SPACE_SIZE, fill=GREEN_COLOR, outline=DARK_GREEN_COLOR, width=5, tag="food"
           )
 
+     # MALCOM
      def place_poison(self):
           unoccupied_cells = set(self.board) - set(self.snake) - set(self.old_food_cell)
           self.poison_cell = random.choice(list(unoccupied_cells))
@@ -163,10 +165,14 @@ class MakanTime:
                x, y, x+SPACE_SIZE, y+SPACE_SIZE, fill=YELLOW_COLOR, outline=ORANGE_COLOR, width=5, tag="poison"
           )
 
+     # RAYNARD
      def display_snake(self, mode=""):
+          ## prevent snake from growing without eating, pop every step
           if self.snake_objects != []:
                self.canvas.delete(self.snake_objects.pop(0))
-          if mode == "full":
+
+          ## initial state of snake
+          if mode == "init":
                for i, cell in enumerate(self.snake):
                     x = cell[0] * SPACE_SIZE
                     y = cell[1] * SPACE_SIZE
@@ -176,15 +182,19 @@ class MakanTime:
                          )
                     )
           else:
-               cell = self.snake[-1]
-               x = cell[0] * SPACE_SIZE
-               y = cell[1] * SPACE_SIZE
+               ## new head
+               head = self.snake[-1]
+               x = head[0] * SPACE_SIZE
+               y = head[1] * SPACE_SIZE
+
+               ## append new head to snake
                self.snake_objects.append(
                          self.canvas.create_rectangle(
                               x, y, x+SPACE_SIZE, y+SPACE_SIZE, fill=SNAKE_COLOR, outline=SNAKE_OUTLINE_COLOR, width=5, tag="head"
                          )
                     )
-               if self.snake[0] == self.old_food_cell:
+               if head == self.old_food_cell:
+                    ## insert new cell at tail
                     self.snake.insert(0, self.old_food_cell)
                     self.old_food_cell = []
                     tail = self.snake[0]
@@ -192,28 +202,35 @@ class MakanTime:
                     y = tail[1] * SPACE_SIZE
                     self.snake_objects.append(
                          self.canvas.create_rectangle(
-                              x, y, x+SPACE_SIZE, y+SPACE_SIZE, fill=SNAKE_COLOR, outline=SNAKE_OUTLINE_COLOR, width=5, tag="head"
+                              x, y, x+SPACE_SIZE, y+SPACE_SIZE, fill=SNAKE_COLOR, outline=SNAKE_OUTLINE_COLOR, width=5, tag="new tail"
                          )
                     )
-               elif self.snake[0] == self.old_poison_cell:
+               elif head == self.old_poison_cell:
+                    ## delete tail
                     self.canvas.delete(self.snake_objects.pop(0))
 
-               self.window.update()
+          self.window.update()
 
      # -------------------------------------------------
-     # Game Logic Functions
+     # Game Logic Function
      # -------------------------------------------------
      
+     # RAYNARD
      def update_snake(self, key):
+
           tail = self.snake[0]
           head = self.snake[-1]
+
+          ## prevent snake from growing without eating anything
           if tail != self.old_food_cell:
                self.snake.pop(0)
-          if tail == self.old_poison_cell:
-               if len(self.snake) == 0:
-                    self.crashed = True
+
+          ## reduce length of snake if it eats poison
+          if head == self.old_poison_cell:
+               if len(self.snake) == 0: self.crashed = True
                else: self.snake.pop(0)
-               
+          
+          ## append new head depending on direction of movement
           if key == "Left":
                self.snake.append((head[0] - 1, head[1]))
           elif key == "Right":
@@ -225,21 +242,29 @@ class MakanTime:
 
           head = self.snake[-1]
           if (
+               ## exceed right side of border
                head[0] > COLS - 1
+               ## exceed left side of border
                or head[0] < 0
+               ## exceed top side of border
                or head[1] > ROWS - 1
+               ## exceed bottom side of border
                or head[1] < 0
+               ## if len not equal means snake body part overlaps
                or len(set(self.snake)) != len(self.snake)
-               or len(self.snake) < 0
+               ## 0 body parts left
+               or len(self.snake) <= 0
           ):
                # hit border/ own body/ or no more body
                self.crashed = True
+
           elif self.food_cell == head:
                # eat food
                self.old_food_cell = self.food_cell
                self.canvas.delete(self.food_obj)
                self.place_food()
                self.display_snake()
+
           elif self.poison_cell == head:
                # eat poison
                self.old_poison_cell = self.poison_cell
@@ -254,24 +279,28 @@ class MakanTime:
      # Input Functions
      # -------------------------------------------------
 
+     # JADEN
      def check_if_key_valid(self, key):
         valid_keys = ["Up", "Down", "Left", "Right"]
+        ## check if valid key in dict
         if key in valid_keys and self.forbidden_actions[self.heading] != key:
             return True
         else:
             return False
      
+     # JADEN
      def mouse_input(self, event):
           self.play_again()
 
+     # JADEN
      def key_input(self, event):
           if not self.crashed:
                key_pressed = event.keysym
 
-               # Check if the pressed key is a valid key
                if self.check_if_key_valid(key_pressed):
-                    # print(key_pressed)
+                    ## only start game if user is ready and presses valid key
                     self.begin = True
+                    ## updates last key only when valid
                     self.last_key = key_pressed
 
 
